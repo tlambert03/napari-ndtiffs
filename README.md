3e066e6dbed86de96e6544c3242613a04be64e60
Talley Lambert <talley.lambert@gmail.com>
2020-05-05 13:16:46 -0400

    update deps

--- README.md
+++ README.md
@@ -12,14 +12,6 @@ napari plugin for nd tiff folders with OpenCl deskew
 
 This [napari] plugin was generated with [Cookiecutter] using with [@napari]'s [cookiecutter-napari-plugin] template.
 
-<!--
-Don't miss the full getting started guide to set up your new package:
-https://github.com/napari/cookiecutter-napari-plugin#getting-started
-
-and review the napari docs for plugin developers:
-https://napari.org/docs/plugins/index.html
--->
-
 ## Installation
 
 You can install `napari-ndtiffs` via [pip]:
@@ -43,12 +35,7 @@ If you encounter any problems, please [file an issue] along with a detailed desc
 [napari]: https://github.com/napari/napari
 [Cookiecutter]: https://github.com/audreyr/cookiecutter
 [@napari]: https://github.com/napari
-[MIT]: http://opensource.org/licenses/MIT
 [BSD-3]: http://opensource.org/licenses/BSD-3-Clause
-[GNU GPL v3.0]: http://www.gnu.org/licenses/gpl-3.0.txt
-[GNU LGPL v3.0]: http://www.gnu.org/licenses/lgpl-3.0.txt
-[Apache Software License 2.0]: http://www.apache.org/licenses/LICENSE-2.0
-[Mozilla Public License 2.0]: https://www.mozilla.org/media/MPL/2.0/index.txt
 [cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
 [file an issue]: https://github.com/tlambert03/napari-ndtiffs/issues
 [napari]: https://github.com/napari/napari
--- setup.py
+++ setup.py
@@ -8,53 +8,43 @@ from setuptools import setup, find_packages
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
-# Add your dependencies in requirements.txt
-# Note: you can add test-specific requirements in tox.ini
 requirements = []
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     for line in f:
         stripped = line.split("#")[0].strip()
         if len(stripped) > 0:
             requirements.append(stripped)
 
-
-# https://github.com/pypa/setuptools_scm
-use_scm = {"write_to": "napari_ndtiffs/_version.py"}
-
 setup(
-    name='napari-ndtiffs',
-    author='Talley Lambert',
-    author_email='talley.lambert@gmail.com',
-    license='BSD-3',
-    url='https://github.com/tlambert03/napari-ndtiffs',
-    description='napari plugin for nd tiff folders with OpenCl deskew',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
+    name="napari-ndtiffs",
+    author="Talley Lambert",
+    author_email="talley.lambert@gmail.com",
+    license="BSD-3",
+    url="https://github.com/tlambert03/napari-ndtiffs",
+    description="napari plugin for nd tiff folders with OpenCl deskew",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=requirements,
-    use_scm_version=use_scm,
-    setup_requires=['setuptools_scm'],
+    use_scm_version={"write_to": "napari_ndtiffs/_version.py"},
+    setup_requires=["setuptools_scm"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: BSD License',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
     ],
-    entry_points={
-        'napari.plugin': [
-            'ndtiffs = napari_ndtiffs',
-        ],
-    },
+    entry_points={"napari.plugin": ["ndtiffs = napari_ndtiffs",],},
 )
